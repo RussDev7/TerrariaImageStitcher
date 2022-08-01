edbdbf4c0d3503efd835e7d50569b74d44e5f294
DannyRuss <33048298+RussDev7@users.noreply.github.com>
2022-08-01 16:16:14 -0700

    Added Files v1.0

---
+++ src/TerrariaImageStitcher/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace TerrariaImageStitcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(392, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stitch Images";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(307, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save Location";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(307, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Add Photos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(289, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 175);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(289, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 254);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(392, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton7);
+            this.groupBox1.Controls.Add(this.radioButton6);
+            this.groupBox1.Controls.Add(this.radioButton5);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 202);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(392, 46);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conversion Format:";
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(330, 19);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(51, 17);
+            this.radioButton7.TabIndex = 6;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "WMF";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(282, 19);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(42, 17);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "GIF";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(225, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(51, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "ICON";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(172, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "EMF";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(118, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "BMP";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(60, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "JPEG";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "PNG";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::TerrariaImageStitcher.Properties.Resources.Terraria_Logo2;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(415, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(412, 343);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TerrariaImageStitcher | Coder: D.RUSS#2430";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ src/TerrariaImageStitcher/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace TerrariaImageStitcher
+{
+    public partial class Form1 : Form
+    {
+
+        // Say Hello To Decompilers
+        private string HelloThere = "Hello there fellow Decompiler, This Program Was Made By D.RU$$ (xXCrypticNightXx).";
+
+        #region Main Code
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string[] PhotosLoc = { "" };
+        public string SaveLoc = "";
+        public int imgwide = 0;
+        public int imgtall = 0;
+
+        // Stitch Photo Function
+        public System.Drawing.Bitmap CombineBitmap(string[] files)
+        {
+
+            //read all images into memory
+            List<System.Drawing.Bitmap> images = new List<System.Drawing.Bitmap>();
+            System.Drawing.Bitmap finalImage = null;
+
+            try
+            {
+
+                bool firstrun = true;
+
+                int imgwide = 0;
+                int imgtall = 0;
+                int width = 0;
+                int height = 0;
+
+                int widecount = 0;
+                int tallcount = 0;
+                bool locktall = false;
+
+                int oldvalue = 0;
+                bool lockit = false;
+                bool dead = false;
+
+                int tempwidth = 1;
+                int temptall = 0;
+
+                int tallcountrunner = 0;
+                int widthcountrunner = 0;
+
+                foreach (string image in files)
+                {
+                    int pos = image.LastIndexOf(@"\") + 1;
+                    string filenumber = image.Substring(pos, image.Length - pos).GetUntilOrEmpty();
+
+                    if (!lockit)
+                    {
+                        // Look For First Number
+                        if (!dead)
+                        {
+                            temptall++;
+                            oldvalue = int.Parse(filenumber);
+                        }
+                        lockit = true;
+                    }
+                    else if (int.Parse(filenumber) != oldvalue)
+                    {
+                        // New Value Changed!
+                        dead = true;
+                        lockit = false;
+                        oldvalue = int.Parse(filenumber);
+                        tempwidth++;
+                    }
+                    else
+                    {
+                        if (!dead)
+                        {
+                            temptall++;
+                        }
+                    }
+                }
+
+                //create a bitmap to hold the combined image
+                finalImage = new System.Drawing.Bitmap((tempwidth * 2048) + 32, (temptall * 2048) + 32);
+
+                // Setup Progress Bar
+                progressBar1.Step = 1;
+                progressBar1.Maximum = (tempwidth * temptall) + 1;
+
+                foreach (string image in files)
+                {
+                    //create a Bitmap from the file and add it to the list
+                    System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(image);
+
+                    //update the size of the final bitmap
+                    width += bitmap.Width;
+                    height = bitmap.Height > height ? bitmap.Height : height;
+
+                    images.Add(bitmap);
+                }
+
+                //get a graphics object from the image so we can draw on it
+                using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(finalImage))
+                {
+                    //set background color
+                    g.Clear(System.Drawing.Color.Transparent);
+
+                    //go through each image and draw it on the final image
+                    foreach (System.Drawing.Bitmap image in images)
+                    {
+
+                        if (widthcountrunner >= (temptall * tempwidth) - temptall) // LAST ROW
+                        {
+                            if (firstrun) // First Row
+                            {
+                                // Update Image First Run
+                                if (temptall <= 1)
+                                {
+                                    g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width, image.Height));
+                                }
+                                else
+                                {
+                                    g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width, image.Height + 32));
+                                }
+
+                                // Update Bool
+                                firstrun = false;
+                            }
+                            else if (tallcountrunner == temptall - 1) // Bottom Row
+                            {
+                                // Reset Count
+                                tallcountrunner = -1;
+
+                                // Update Image First Run
+                                g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width, image.Height));
+                            }
+                            else // Everything Else
+                            {
+                                // Update Image Each Time After
+                                g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width, image.Height + 32));
+                            }
+                        }
+                        else
+                        { // Everything EXCEPT LAST ROW
+                            if (firstrun) // First Row
+                            {
+                                // Update Image First Run
+                                if (temptall <= 1)
+                                {
+                                    g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width, image.Height));
+                                }
+                                else
+                                {
+                                    g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width + 32, image.Height + 32));
+                                }
+
+                                // Update Bool
+                                firstrun = false;
+                            }
+                            else if (tallcountrunner == temptall - 1) // Bottom Row
+                            {
+                                // Reset Count
+                                tallcountrunner = -1;
+
+                                // Update Image First Run
+                                g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width + 32, image.Height));
+                            }
+                            else // Everything Else
+                            {
+                                // Update Image Each Time After
+                                g.DrawImage(image, new System.Drawing.Rectangle(imgwide, imgtall, image.Width + 32, image.Height + 32));
+                            }
+                        }
+
+                        // Manage Y
+                        if (imgtall >= ((2048 * temptall) - 2048))
+                        {
+                            // Update Firstrun
+                            firstrun = true;
+
+                            imgtall = 0;
+                            imgwide = imgwide + 2048;
+
+                            // For Cropping Tall
+                            if (!locktall)
+                            {
+                                tallcount = tallcount + image.Height;
+                                locktall = true;
+                            }
+
+                            // For Cropping Wide
+                            widecount = widecount + image.Width;
+
+                            // Progress Progressbar
+                            progressBar1.PerformStep();
+
+                        }
+                        else
+                        {
+                            imgtall = imgtall + 2048;
+
+                            // For Cropping
+                            if (!locktall)
+                            {
+                                tallcount = tallcount + image.Height;
+                            }
+
+                            // Progress Progressbar
+                            progressBar1.PerformStep();
+
+                        }
+                        tallcountrunner++;
+                        widthcountrunner++;
+                    }
+                }
+
+                // Crop Image
+                Bitmap finalImagecroped = new System.Drawing.Bitmap(widecount, tallcount);
+                finalImagecroped = finalImage.Clone(new Rectangle(0, 0, widecount, tallcount), PixelFormat.DontCare);
+
+                // Progress Progressbar
+                progressBar1.PerformStep();
+
+                return finalImagecroped;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally
+            {
+                //clean up memory
+                foreach (System.Drawing.Bitmap image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+
+        // Stitch The File
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            // Check If Locations Are Populated
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("ERROR: Please Add Some Photos To Stitch!");
+                return;
+            }
+            else if (!textBox1.Text.Contains(","))
+            {
+                MessageBox.Show("ERROR: Please Select At Least Two Photos!");
+                return;
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("ERROR: Please Add A Save Location!");
+                return;
+            }
+            else
+            {
+
+                // Reset ProgressBar
+                progressBar1.Value = 0;
+
+                // Convert Bitmap
+                Bitmap final = CombineBitmap(PhotosLoc);
+
+                // Save Bitmap
+                if (radioButton1.Checked)
+                {
+                    final.Save(SaveLoc + ".png", ImageFormat.Png);
+                }
+                else if (radioButton2.Checked)
+                {
+                    final.Save(SaveLoc + ".jpg", ImageFormat.Jpeg);
+                }
+                else if (radioButton3.Checked)
+                {
+                    final.Save(SaveLoc + ".bmp", ImageFormat.Bmp);
+                }
+                else if (radioButton4.Checked)
+                {
+                    final.Save(SaveLoc + ".emf", ImageFormat.Emf);
+                }
+                else if (radioButton5.Checked)
+                {
+                    final.Save(SaveLoc + ".ico", ImageFormat.Icon);
+                }
+                else if (radioButton6.Checked)
+                {
+                    final.Save(SaveLoc + ".gif", ImageFormat.Gif);
+                }
+                else if (radioButton7.Checked)
+                {
+                    final.Save(SaveLoc + ".wmf", ImageFormat.Wmf);
+                }
+
+                // Job Finished
+                MessageBox.Show("Stitch Has Completed!");
+
+            }
+        }
+
+        // Get Save Location
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                Title = "Stitched Photo Save Name"
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                SaveLoc = dialog.FileName;
+                textBox2.Text = dialog.FileName;
+            }
+        }
+
+        // Get Photos Loc
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog x = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = "Png Files|*.png",
+                Title = "Select Photos To Stitch"
+            };
+            x.ShowDialog();
+            PhotosLoc = x.FileNames;
+
+            int PhotoCount = 0;
+
+            // Sort Textbox
+            foreach (string s in x.FileNames)
+            {
+                if (textBox1.Text == "")
+                {
+                    // Get Dir Count
+                    PhotoCount++;
+                    textBox1.Text = s;
+                }
+                else
+                {
+                    // Get Dir Count
+                    PhotoCount++;
+                    textBox1.Text = s + ", " + textBox1.Text;
+                }
+            }
+
+        }
+
+    }
+
+    static class Helper
+    {
+        public static string GetUntilOrEmpty(this string text, string stopAt = "-")
+        {
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+
+                if (charLocation > 0)
+                {
+                    return text.Substring(0, charLocation);
+                }
+            }
+
+            return String.Empty;
+        }
+
+        #endregion
+
+    }
+
+}
